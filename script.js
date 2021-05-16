cd8cea0f06324f258f7ff87ab72a5bc6aa1c0978
Billal Siddiqi <billalsiddiqi@yahoo.com>
2021-05-16 19:13:23 +0430

    Save Selections into local Storage

--- script.js
+++ script.js
@@ -4,17 +4,50 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('select');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('movieIndex', movieIndex);
+    localStorage.setItem('moviePrice', moviePrice)
+}
 
 function countSelectedSeats() {
     const selectedSeats = document.querySelectorAll('.single-seat.select');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     count.innerHTML = selectedSeats.length
     total.innerHTML = selectedSeats.length * ticketPrice;
 }
 
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('select');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('movieIndex');
+
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+
+    setMovieData(e.target.selectedIndex, e.target.value);
     countSelectedSeats();
 })
 
@@ -25,3 +58,5 @@ container.addEventListener('click', (e) => {
         countSelectedSeats()
     }
 })
+
+countSelectedSeats();
